a608225de05e45e6864e2564324936b364b4023e
nikberX <hemenbgmb@gmail.com>
2021-07-20 00:19:21 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+//dart imports
+
+//packages
+import 'package:flutter/material.dart';
+
+//files
+import './screens/sign_up_screen.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TestTask',
+      theme: ThemeData(
+        textTheme: TextTheme(
+          title: TextStyle(
+            fontFamily: 'Roboto',
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        )
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+    @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SignUpScreen();
+  }
+}
